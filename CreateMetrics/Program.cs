9dc4bf64d07b9bc6a8edd149f6bb65c6211a46ce
mli2805 <mli2805@gmail.com>
2023-04-05 14:23:59 +0300

    Add project files.

---
+++ CreateMetrics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+var performanceCounterCpuLoad = new PerformanceCounter("Processor Information", "% Processor Utility", "_Total");
+
+// call this every second or so
+while (true)
+{
+    var percent = (int)performanceCounterCpuLoad.NextValue();
+    Console.WriteLine(percent);
+    await Task.Delay(1000);
+}
